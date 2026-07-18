9d635d90a9115deece066d074e6808b51300d676
R Ajit <164767132+RaoAjit@users.noreply.github.com>
2026-07-18 10:44:04 +0530

    Update settings.py

--- myolx/settings.py
+++ myolx/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p1)kfgh3f8!%x!)=-guke!&y=zwm5rkcps%^#236swt*vt=3me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "olxsite.vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
